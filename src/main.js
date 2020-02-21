3950e9161dbea2f7388e62d1f80ab8b9267e6072
unknown <ahmedhamouda399@gmail.com>
2020-02-21 19:04:50 +0200

    fix Bilod

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import store from "./store"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import vuetify from './plugins/vuetify'
+
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  store,
+  render: h => h(App)
 }).$mount('#app')
